25765e9f619e82c3421b4d940ea497e11ecb540d
devulogi <devulogijrp.github@gmail.com>
2024-02-18 07:34:05 +0800

    Add dice roll functionality

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 """ This is the main module."""
 from starter_projects.madlibs import madlibs
 from starter_projects.number_guessing import guessing_game
+from starter_projects.dice_simulator import dice_roll
 
 if __name__ == "__main__":
-    guessing_game()
+    dice_roll()
